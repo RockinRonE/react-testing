b33bb784227920025b58084acd3041743fb2b697
Ronny Ewanek <ronny.ewanek@gmail.com>
2018-05-21 10:31:54 -0500

    add reducer tests

--- flashcardpro/src/actions/index.test.js
+++ flashcardpro/src/actions/index.test.js
@@ -10,4 +10,17 @@ describe('actions', () => {
 
     expect(actions.setStack(stack)).toEqual(expectedAction)
   })
+
+  it('creates an action to add a stack', () => {
+    const expectedAction = {
+      type: actions.ADD_STACK,
+      stack
+    }
+    expect(actions.addStack(stack)).toEqual(expectedAction)
+  })
+
+  it('creates an action to load stacks', () => {
+    const expectedAction = { type: actions.LOAD_STACKS, stacks }
+    expect(actions.loadStacks(stacks)).toEqual(expectedAction)
+  })
 })
---
+++ flashcardpro/src/reducers/index.test.js
@@ -0,0 +1,22 @@
+import rootReducer from './index'
+import * as actions from '../actions'
+import { stack, stacks } from '../data/fixtures'
+
+describe('root reducer', () => {
+  it('returns the initial state', () => {
+    // prevState, action object
+    expect(rootReducer({}, {})).toEqual({ stack: {}, stacks: [] })
+  })
+
+  it('sets the main stack', () => {
+    expect(rootReducer({}, { type: actions.SET_STACK, stack })).toEqual({ stack, stacks: [] })
+  })
+
+  it('loads stacks', () => {
+    expect(rootReducer({}, { type: actions.LOAD_STACKS, stacks })).toEqual({ stack: {}, stacks })
+  })
+
+  it('adds a stack', () => {
+    expect(rootReducer({}, { type: actions.ADD_STACK, stack })).toEqual({ stack: {}, stacks: [stack] })
+  })
+})
